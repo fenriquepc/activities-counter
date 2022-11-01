319442672bc9299e037f5723140572ded8252b2d
fenriquepc <fenriquepc@gmail.com>
2022-11-01 15:25:51 +0100

    Move files

--- ActivitiesCounter/Entities/Activity.cs
+++ ActivitiesCounter/Entities/Activity.cs
@@ -6,7 +6,9 @@
         public string Description { get; set; }
         public Game Game { get; set; }
         public DateTime Date { get; set; }
-        public int Players { get; set; }
+        public int Capacity { get; set; }
+        public int Reserved { get; set; }
+        public int AvailableCapacity { get=> Capacity - Reserved; }
         public ActivityType Type { get; set; }
         public ActivityLevel Level { get; set; }
         public Organizer Organizer { get; set; }
---
+++ ActivitiesCounter/Program.cs
@@ -0,0 +1,25 @@
+using ActivitiesCounter;
+using ActivitiesCounter.Managers;
+using ActivitiesCounter.Repositories;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<IFilesManager, FilesManager>();
+builder.Services.AddScoped<IActivitiesManager, ActivitiesManager>();
+builder.Services.AddScoped<IActivityRepository, ActivityRepository>();
+
+var host =  builder.Build();
+
+var filesManager = host.Services.GetRequiredService<IFilesManager>();
+filesManager.LoadFilesData();
+
+await host.RunAsync();
