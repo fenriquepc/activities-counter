7c9fc24bd1e551de1791baae9dac0324d0d43b20
fenriquepc <fenriquepc@gmail.com>
2023-03-04 09:24:28 +0100

    FIx participants deserialization

--- ActivitiesCounter/Entities/Activity.cs
+++ ActivitiesCounter/Entities/Activity.cs
@@ -4,24 +4,22 @@ namespace ActivitiesCounter.Entities;
 
 public class Activity
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public string Game { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public DateTime Date { get; set; }
-    public int Capacity { get; set; }
-    public IEnumerable<string> Participants => _participants;
-    public int AvailableCapacity => Capacity - _participants.Count;
-    public ActivityType Type { get; set; }
-    public ActivityLevel Level { get; set; }
-    public Organizer Organizer { get; set; }
-    public bool AdultsOnly { get; set; }
-    public bool AllowPreinscription { get; set; }
-    [JsonPropertyName("participants")] 
-    private IList<string> _participants = new List<string>();
+	public Guid Id { get; set; } = Guid.NewGuid();
+	public string Game { get; set; }
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public DateTime Date { get; set; }
+	public int Capacity { get; set; }
+	public List<string> Participants { get; set; } = new();
+	public int AvailableCapacity => Capacity - Participants.Count;
+	public ActivityType Type { get; set; }
+	public ActivityLevel Level { get; set; }
+	public Organizer Organizer { get; set; }
+	public bool AdultsOnly { get; set; }
+	public bool AllowPreinscription { get; set; }
 
-    public void AddParticipant(string participant)
-    {
-        _participants.Add(participant);
-    }
+	public void AddParticipant(string participant)
+	{
+		Participants.Add(participant);
+	}
 }
