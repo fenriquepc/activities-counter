0d9ebe410b986feaced0645c02155370bdf36bd8
fenriquepc <fenriquepc@gmail.com>
2023-03-04 09:01:08 +0100

    Spacing formatting

--- ActivitiesCounter/Managers/ActivitiesManager.cs
+++ ActivitiesCounter/Managers/ActivitiesManager.cs
@@ -5,25 +5,25 @@ namespace ActivitiesCounter.Managers;
 
 public class ActivitiesManager : IActivitiesManager
 {
-    private readonly IActivityRepository _activityRepository;
+	private readonly IActivityRepository _activityRepository;
 
-    public ActivitiesManager(IActivityRepository activityRepository)
-    {
-        _activityRepository = activityRepository;
-    }
+	public ActivitiesManager(IActivityRepository activityRepository)
+	{
+		_activityRepository = activityRepository;
+	}
 
-    public async Task<IEnumerable<Activity>> GetNextActivitiesAsync()
-    {
-        var activities = await _activityRepository.GetAll();
-        return activities.OrderByDescending(a => a.Date);
-    }
+	public async Task<IEnumerable<Activity>> GetNextActivitiesAsync()
+	{
+		var activities = await _activityRepository.GetAll();
+		return activities.OrderByDescending(a => a.Date);
+	}
 
-    public Task UpsertActivityAsync(Activity activity) 
-        => _activityRepository.UpsertActivity(activity);
+	public Task UpsertActivityAsync(Activity activity) 
+		=> _activityRepository.UpsertActivity(activity);
 
-    public Task AddParticipantAsync(Activity activity, string participant)
-    {
-        activity.AddParticipant(participant);
-        return UpsertActivityAsync(activity);
-    }
+	public Task AddParticipantAsync(Activity activity, string participant)
+	{
+		activity.AddParticipant(participant);
+		return UpsertActivityAsync(activity);
+	}
 }
--- ActivitiesCounter/Repositories/ActivityRepository.cs
+++ ActivitiesCounter/Repositories/ActivityRepository.cs
@@ -5,38 +5,38 @@ namespace ActivitiesCounter.Repositories;
 
 public class ActivityRepository : IActivityRepository
 {
-    private const string ACTIVITIES_KEY = "activities";
-    private readonly ILocalStorageService _localStorage;
+	private const string ACTIVITIES_KEY = "activities";
+	private readonly ILocalStorageService _localStorage;
 
-    public ActivityRepository(ILocalStorageService localStorage)
-    {
-        _localStorage = localStorage;
-    }
+	public ActivityRepository(ILocalStorageService localStorage)
+	{
+		_localStorage = localStorage;
+	}
 
-    public async ValueTask<IEnumerable<Activity>> GetAll()
-    {
-        var result = await _localStorage.GetItemAsync<IEnumerable<Activity>>(ACTIVITIES_KEY); 
-        return result ?? Enumerable.Empty<Activity>();
-    }
+	public async ValueTask<IEnumerable<Activity>> GetAll()
+	{
+		var result = await _localStorage.GetItemAsync<IEnumerable<Activity>>(ACTIVITIES_KEY); 
+		return result ?? Enumerable.Empty<Activity>();
+	}
 
-    public ValueTask RemoveAll()
-        => _localStorage.RemoveItemAsync(ACTIVITIES_KEY);
+	public ValueTask RemoveAll()
+		=> _localStorage.RemoveItemAsync(ACTIVITIES_KEY);
 
-    public async Task UpsertActivity(Activity activity)
-    {
-        var activities = (await GetAll()).ToList() ?? new List<Activity>();
-        var existingActivityIndex = activities.FindIndex(a => a.Id == activity.Id);
+	public async Task UpsertActivity(Activity activity)
+	{
+		var activities = (await GetAll()).ToList() ?? new List<Activity>();
+		var existingActivityIndex = activities.FindIndex(a => a.Id == activity.Id);
 
-        if (existingActivityIndex < 0)
-        {
-            activities.Add(activity);
-        }
-        else 
-            activities[existingActivityIndex] = activity;
+		if (existingActivityIndex < 0)
+		{
+			activities.Add(activity);
+		}
+		else 
+			activities[existingActivityIndex] = activity;
 
-        await PersistActivities(activities);
-    }
+		await PersistActivities(activities);
+	}
 
-    private ValueTask PersistActivities(IEnumerable<Activity> activities)
-        => _localStorage.SetItemAsync(ACTIVITIES_KEY, activities);
+	private ValueTask PersistActivities(IEnumerable<Activity> activities)
+		=> _localStorage.SetItemAsync(ACTIVITIES_KEY, activities);
 }
