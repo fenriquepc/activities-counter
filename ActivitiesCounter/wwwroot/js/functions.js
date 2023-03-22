10a1ae2488de59491d250a71efc6a090c5a056ed
fenriquepc <fenriquepc@gmail.com>
2023-03-22 22:13:17 +0100

    Capacity counter fixes

--- ActivitiesCounter/wwwroot/js/functions.js
+++ ActivitiesCounter/wwwroot/js/functions.js
@@ -1,5 +1,15 @@
 function Collapse(elementId) {
     if (document.getElementById(elementId) !== null) {
-        new bootstrap.Collapse(document.getElementById(elementId)).toggle();
+        new bootstrap.Collapse(document.getElementById(elementId), {
+            toggle: false
+        }).toggle();
+    }
+}
+
+function Hide(elementId) {
+    if (document.getElementById(elementId) !== null) {
+        new bootstrap.Collapse(document.getElementById(elementId), {
+            toggle: false
+        }).hide();
     }
 }
